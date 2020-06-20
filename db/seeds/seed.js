d3225128819fe4f968b7ac9fe78f0d4c7f7e8553
Kabi <kabilan.siv@gmail.com>
2020-06-20 23:41:46 +0200

    Author: Kabilan Sivanamam State: Partially working Done: Improved table names and attributes. Next: Adjust API to new DB alterations.

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -4,386 +4,386 @@ exports.seed = async function (knex) {
 	// Deletes ALL existing entries
 	await knex('users').del()
 	await knex('workouts').del()
-	await knex('exerciseLibrary').del()
+	await knex('exercises').del()
 	await knex('workoutExercises').del()
 	await knex('plans').del()
 
 	await knex('users').insert([{
-			username: 'admin',
-			password: await bcrypt.hash('admin', 10),
-			email: 'admin@calix.ch',
-			firstname: 'admin',
-			lastname: 'admin',
-			yearOfBirth: 1,
-			height: 1,
-			weight: 1,
-			equipment: false,
-			color: 'orange',
-			adminAuthorization: true
+			user_username: 'admin',
+			user_password: await bcrypt.hash('admin', 10),
+			user_email: 'admin@calix.ch',
+			user_firstname: 'admin',
+			user_lastname: 'admin',
+			user_yearOfBirth: 1,
+			user_height: 1,
+			user_weight: 1,
+			user_equipment: false,
+			user_color: 'orange',
+			user_adminAuthorization: true
 		},
 		{
-			username: 'kasi',
-			password: await bcrypt.hash('admin', 10),
-			email: 'kasi@tfbern.ch',
-			firstname: 'Kabilan',
-			lastname: 'Sivanamam',
-			yearOfBirth: 1998,
-			height: 171,
-			weight: 70,
-			equipment: false,
-			color: 'orange',
-			adminAuthorization: false
+			user_username: 'kasi',
+			user_password: await bcrypt.hash('admin', 10),
+			user_email: 'kasi@tfbern.ch',
+			user_firstname: 'Kabilan',
+			user_lastname: 'Sivanamam',
+			user_yearOfBirth: 1998,
+			user_height: 171,
+			user_weight: 70,
+			user_equipment: false,
+			user_color: 'orange',
+			user_adminAuthorization: false
 		},
 		{
-			username: 'doke',
-			password: await bcrypt.hash('admin', 10),
-			email: 'doke@tfbern.ch',
-			firstname: 'Dominik',
-			lastname: 'Keller',
-			yearOfBirth: 2001,
-			height: 180,
-			weight: 75,
-			equipment: false,
-			color: 'Green',
-			adminAuthorization: false
+			user_username: 'doke',
+			user_password: await bcrypt.hash('admin', 10),
+			user_email: 'doke@tfbern.ch',
+			user_firstname: 'Dominik',
+			user_lastname: 'Keller',
+			user_yearOfBirth: 2001,
+			user_height: 180,
+			user_weight: 75,
+			user_equipment: false,
+			user_color: 'Green',
+			user_adminAuthorization: false
 		},
 		{
-			username: 'alul',
-			password: await bcrypt.hash('admin', 10),
-			email: 'alul@tfbern.ch',
-			firstname: 'Ali',
-			lastname: 'Ülgen',
-			yearOfBirth: 1999,
-			height: 185,
-			weight: 100,
-			equipment: false,
-			color: 'red',
-			adminAuthorization: false
+			user_username: 'alul',
+			user_password: await bcrypt.hash('admin', 10),
+			user_email: 'alul@tfbern.ch',
+			user_firstname: 'Ali',
+			user_lastname: 'Ülgen',
+			user_yearOfBirth: 1999,
+			user_height: 185,
+			user_weight: 100,
+			user_equipment: false,
+			user_color: 'red',
+			user_adminAuthorization: false
 		},
 		{
-			username: 'bahe',
-			password: await bcrypt.hash('admin', 10),
-			email: 'bahe@tfbern.ch',
-			firstname: 'Hezekiah',
-			lastname: 'Bakare-Johnson',
-			yearOfBirth: 1999,
-			height: 185,
-			weight: 100,
-			equipment: false,
-			color: 'red',
-			adminAuthorization: false
+			user_username: 'bahe',
+			user_password: await bcrypt.hash('admin', 10),
+			user_email: 'bahe@tfbern.ch',
+			user_firstname: 'Hezekiah',
+			user_lastname: 'Bakare-Johnson',
+			user_yearOfBirth: 1999,
+			user_height: 185,
+			user_weight: 100,
+			user_equipment: false,
+			user_color: 'red',
+			user_adminAuthorization: false
 		},
 		{
-			username: 'test',
-			password: await bcrypt.hash('test', 10),
-			email: 'test@tfbern.ch',
-			firstname: 'test',
-			lastname: 'test-test',
-			yearOfBirth: 1999,
-			height: 185,
-			weight: 100,
-			equipment: false,
-			color: 'red',
-			adminAuthorization: false
+			user_username: 'test',
+			user_password: await bcrypt.hash('test', 10),
+			user_email: 'test@tfbern.ch',
+			user_firstname: 'test',
+			user_lastname: 'test-test',
+			user_yearOfBirth: 1999,
+			user_height: 185,
+			user_weight: 100,
+			user_equipment: false,
+			user_color: 'red',
+			user_adminAuthorization: false
 		}
 	])
 
 	await knex('workouts').insert([{
-			name: 'A',
-			focus: 'Arms & Back',
-			difficulty: 2,
-			userFK: 1,
-			public: true
+			workout_name: 'A',
+			workout_focus: 'Arms & Back',
+			workout_difficulty: 2,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'B',
-			focus: 'running',
-			difficulty: 3,
-			userFK: 1,
-			public: true
+			workout_name: 'B',
+			workout_focus: 'running',
+			workout_difficulty: 3,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'C',
-			focus: 'running',
-			difficulty: 3,
-			userFK: 1,
-			public: true
+			workout_name: 'C',
+			workout_focus: 'running',
+			workout_difficulty: 3,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'D',
-			focus: 'running',
-			difficulty: 3,
-			userFK: 1,
-			public: true
+			workout_name: 'D',
+			workout_focus: 'running',
+			workout_difficulty: 3,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'E',
-			focus: 'running',
-			difficulty: 3,
-			userFK: 1,
-			public: true
+			workout_name: 'E',
+			workout_focus: 'running',
+			workout_difficulty: 3,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'F',
-			focus: 'running',
-			difficulty: 3,
-			userFK: 1,
-			public: true
+			workout_name: 'F',
+			workout_focus: 'running',
+			workout_difficulty: 3,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'G',
-			focus: 'running',
-			difficulty: 3,
-			userFK: 1,
-			public: true
+			workout_name: 'G',
+			workout_focus: 'running',
+			workout_difficulty: 3,
+			workout_user_FK: 1,
+			workout_public: true
 		},
 		{
-			name: 'H',
-			focus: 'flexibility',
-			difficulty: 1,
-			userFK: 6,
-			public: false
+			workout_name: 'H',
+			workout_focus: 'flexibility',
+			workout_difficulty: 1,
+			workout_user_FK: 6,
+			workout_public: false
 		}
 	])
 
-	await knex('exerciseLibrary').insert([{
-			name: 'Pull Up',
-			weighted: false,
-			advanced: false,
-			engagement: 'Upper Back',
-			type: 'Strength',
-			public: true,
-			userFK: 1
+	await knex('exercises').insert([{
+			exercise_name: 'Pull Up',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Upper Back',
+			exercise_type: 'Strength',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: 'Push Up',
-			weighted: false,
-			advanced: false,
-			engagement: 'Triceps',
-			type: 'Strength',
-			public: true,
-			userFK: 1
+			exercise_name: 'Push Up',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Triceps',
+			exercise_type: 'Strength',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: 'Chin Up',
-			weighted: false,
-			advanced: false,
-			engagement: 'Biceps',
-			type: 'Strength',
-			public: true,
-			userFK: 1
+			exercise_name: 'Chin Up',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Biceps',
+			exercise_type: 'Strength',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '100m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '100m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '200m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '200m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '300m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '300m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '400m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '400m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '500m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '500m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '600m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '600m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '700m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '700m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '800m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '800m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '900m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '900m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '1000m running',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Speed',
-			public: true,
-			userFK: 1
+			exercise_name: '1000m running',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Speed',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '2km jogging',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Endurance',
-			public: true,
-			userFK: 1
+			exercise_name: '2km jogging',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Endurance',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '4km jogging',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Endurance',
-			public: true,
-			userFK: 1
+			exercise_name: '4km jogging',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Endurance',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '6km jogging',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Endurance',
-			public: true,
-			userFK: 1
+			exercise_name: '6km jogging',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Endurance',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '8km jogging',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Endurance',
-			public: true,
-			userFK: 1
+			exercise_name: '8km jogging',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Endurance',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: '10km jogging',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Endurance',
-			public: true,
-			userFK: 1
+			exercise_name: '10km jogging',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Endurance',
+			exercise_public: true,
+			exercise_user_FK: 1
 		},
 		{
-			name: 'Split',
-			weighted: false,
-			advanced: false,
-			engagement: 'Legs',
-			type: 'Flexibility',
-			public: false,
-			userFK: 6
+			exercise_name: 'Split',
+			exercise_weighted: false,
+			exercise_advanced: false,
+			exercise_engagement: 'Legs',
+			exercise_type: 'Flexibility',
+			exercise_public: false,
+			exercise_user_FK: 6
 		},
 	])
 
 	await knex('workoutExercises').insert([{
-			exerciseLibraryFK: 1,
-			sets: 2,
-			reps: 10,
-			workoutFK: 1
+			workoutExercise_exercise_FK: 1,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 1
 		},
 		{
-			exerciseLibraryFK: 2,
-			sets: 2,
-			reps: 30,
-			workoutFK: 1
+			workoutExercise_exercise_FK: 2,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 30,
+			workoutExercise_workout_FK: 1
 		},
 		{
-			exerciseLibraryFK: 3,
-			sets: 2,
-			reps: 10,
-			workoutFK: 1
+			workoutExercise_exercise_FK: 3,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 1
 		},
 		{
-			exerciseLibraryFK: 4,
-			sets: 2,
-			reps: 10,
-			workoutFK: 2
+			workoutExercise_exercise_FK: 4,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 2
 		},
 		{
-			exerciseLibraryFK: 5,
-			sets: 2,
-			reps: 10,
-			workoutFK: 3
+			workoutExercise_exercise_FK: 5,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 3
 		},
 		{
-			exerciseLibraryFK: 6,
-			sets: 2,
-			reps: 10,
-			workoutFK: 4
+			workoutExercise_exercise_FK: 6,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 4
 		},
 		{
-			exerciseLibraryFK: 7,
-			sets: 2,
-			reps: 10,
-			workoutFK: 5
+			workoutExercise_exercise_FK: 7,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 5
 		},
 		{
-			exerciseLibraryFK: 8,
-			sets: 2,
-			reps: 10,
-			workoutFK: 6
+			workoutExercise_exercise_FK: 8,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 6
 		},
 		{
-			exerciseLibraryFK: 9,
-			sets: 2,
-			reps: 10,
-			workoutFK: 7
+			workoutExercise_exercise_FK: 9,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 7
 		},
 		{
-			exerciseLibraryFK: 19,
-			sets: 2,
-			reps: 10,
-			workoutFK: 8
+			workoutExercise_exercise_FK: 19,
+			workoutExercise_sets: 2,
+			workoutExercise_reps: 10,
+			workoutExercise_workout_FK: 8
 		}
 	])
 
 	await knex('plans').insert([{
-		plansUserFK: 6,
-		workoutFK: 1,
-		day: 0
+		plan_user_FK: 6,
+		plan_workout_FK: 1,
+		plan_day: 0
 	}])
 };
